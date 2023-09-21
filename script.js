718edf71a4faebeaf3fc7ba9547feb45a275e28f
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-09-21 13:40:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+const h3 = document.getElementById("wordCount");
+const input = document.getElementById("evaluatedText");
+let count = 0;
+h3.innerText = count;
+input.addEventListener("keydown",(event)=>{
+	let key = event.key;
+	if(key === " "){
+		count++;
+		h3.innerText = count;
+	}
+})
